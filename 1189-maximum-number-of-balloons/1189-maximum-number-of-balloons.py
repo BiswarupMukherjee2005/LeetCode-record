33e98fb6a78b131ec1918a99448d7c45c6b7a526
Biswarup Mukherjee <biswarupmukherjee2005@gmail.com>
2026-06-22 14:29:24 +0530

    Time: 4 ms (22.22%), Space: 19.2 MB (75.19%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -0,0 +1,18 @@
+#translated using AI
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        b = a = l = o = n = 0
+
+        for c in text:
+            if c == 'b':
+                b += 1
+            elif c == 'a':
+                a += 1
+            elif c == 'l':
+                l += 1
+            elif c == 'o':
+                o += 1
+            elif c == 'n':
+                n += 1
+
+        return min(b, a, n, l // 2, o // 2)
